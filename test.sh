bf2a86e801b37e74a6a93be48459c97d646cf5e7
kimathikaai <kiprotich.kaai@gmail.com>
2023-03-05 21:07:39 -0500

    Updating shells scripts

--- test.sh
+++ test.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+# CUDA_VISIBLE_DEVICES=2 DATA_DIR=/pub2/data/ python -m unittest domainbed.test.test_datasets.TestOverlapDatasets
 CUDA_VISIBLE_DEVICES=2 DATA_DIR=/pub2/data/ python -m unittest domainbed.test.test_datasets.TestOverlapDatasets
+CUDA_VISIBLE_DEVICES=2 DATA_DIR=/pub2/data/ python -m unittest domainbed.test.test_model_selection.TestSelectionMethod
+CUDA_VISIBLE_DEVICES=2 DATA_DIR=/pub2/data/ python -m unittest domainbed.test.test_model_selection.TestIIDAccuracySelectionMethod
