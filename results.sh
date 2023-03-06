eee67bc0da4d9e90035a47f94d38f48bb0acfad4
kimathikaai <kiprotich.kaai@gmail.com>
2023-03-05 21:07:39 -0500

    Updating shells scripts

--- results.sh
+++ results.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=2 python -m domainbed.scripts.collect_results\
-	--input_dir=/pub2/tmp
+echo "*****************************0"
+python -m domainbed.scripts.collect_results\
+	--input_dir=/pub2/tmp/0 \
+	--latex
+
+echo "*****************************33"
+python -m domainbed.scripts.collect_results\
+	--input_dir=/pub2/tmp/33 \
+	--latex
+
+echo "*****************************66"
+python -m domainbed.scripts.collect_results\
+	--input_dir=/pub2/tmp/66 \
+	--latex
