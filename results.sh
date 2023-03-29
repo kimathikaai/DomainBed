0896c3ac87279dec3fe14451b856550390ef6bc6
kimathikaai <kiprotich.kaai@gmail.com>
2023-03-28 22:50:16 -0400

    Refactor latex generation

--- results.sh
+++ results.sh
@@ -1,45 +1,47 @@
 #!/bin/bash
 
-overlap=0
-dataset='PACS'
-input_dir='/pub2/podg/'
+dataset='All'
 
-selec_metric='acc'
-eval_metric='acc'
-python -m domainbed.scripts.collect_results\
-	--input_dir=$input_dir \
-    --eval_metric=$eval_metric \
-    --selec_metric=$selec_metric \
-    --overlap=$overlap \
-    --dataset=$dataset \
-	--latex
+for overlap in 0 33 66 100
+do
+    input_dir='~/scratch/saved/domainbed_results/'$overlap
+    selec_metric='acc'
+    eval_metric='acc'
+    python -m domainbed.scripts.collect_results\
+        --input_dir=$input_dir \
+        --eval_metric=$eval_metric \
+        --selec_metric=$selec_metric \
+        --overlap=$overlap \
+        --dataset=$dataset \
+        --latex
 
-selec_metric='acc'
-eval_metric='f1'
-python -m domainbed.scripts.collect_results\
-	--input_dir=$input_dir \
-    --eval_metric=$eval_metric \
-    --selec_metric=$selec_metric \
-    --overlap=$overlap \
-    --dataset=$dataset \
-	--latex
+    selec_metric='acc'
+    eval_metric='f1'
+    python -m domainbed.scripts.collect_results\
+        --input_dir=$input_dir \
+        --eval_metric=$eval_metric \
+        --selec_metric=$selec_metric \
+        --overlap=$overlap \
+        --dataset=$dataset \
+        --latex
 
-selec_metric='acc'
-eval_metric='oacc'
-python -m domainbed.scripts.collect_results\
-	--input_dir=$input_dir \
-    --eval_metric=$eval_metric \
-    --selec_metric=$selec_metric \
-    --overlap=$overlap \
-    --dataset=$dataset \
-	--latex
+    selec_metric='acc'
+    eval_metric='oacc'
+    python -m domainbed.scripts.collect_results\
+        --input_dir=$input_dir \
+        --eval_metric=$eval_metric \
+        --selec_metric=$selec_metric \
+        --overlap=$overlap \
+        --dataset=$dataset \
+        --latex
 
-selec_metric='acc'
-eval_metric='nacc'
-python -m domainbed.scripts.collect_results\
-	--input_dir=$input_dir \
-    --eval_metric=$eval_metric \
-    --selec_metric=$selec_metric \
-    --overlap=$overlap \
-    --dataset=$dataset \
-	--latex
+    selec_metric='acc'
+    eval_metric='nacc'
+    python -m domainbed.scripts.collect_results\
+        --input_dir=$input_dir \
+        --eval_metric=$eval_metric \
+        --selec_metric=$selec_metric \
+        --overlap=$overlap \
+        --dataset=$dataset \
+        --latex
+done
