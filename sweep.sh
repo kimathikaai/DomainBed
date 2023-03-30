f8c846e494a879bcc89c0b603de94bf8635ec3d0
kimathikaai <kiprotich.kaai@gmail.com>
2023-03-29 20:37:42 -0400

    Simplify sweep script

--- sweep.sh
+++ sweep.sh
@@ -11,145 +11,21 @@ gpu=0
 overlap=0
 
 jobid=$(date +"%y%m%d%H%M%S")
-algorithm=ERM
-current_output_dir=${outputdir}/${jobid}/${overlap}/${algorithm}_${datasets}_o${overlap}_h${n_hparams}_s${steps}_t${trial}_${jobid}
-echo starting ${current_output_dir}
-mkdir -p ${current_output_dir}
+for algorithm in XDom Intra Supcon
+do
+    current_output_dir=${outputdir}/${jobid}/${overlap}/${algorithm}_${datasets}_o${overlap}_h${n_hparams}_s${steps}_t${trial}_${jobid}
+    echo starting ${current_output_dir}
+    mkdir -p ${current_output_dir}
 
-echo 'y' |CUDA_VISIBLE_DEVICES=${gpu} python -m domainbed.scripts.sweep launch\
-       --data_dir=${datadir} \
-       --output_dir=${current_output_dir} \
-       --command_launcher local \
-       --overlap ${overlap} \
-       --steps ${steps} \
-       --single_test_envs \
-       --algorithms ${algorithm} \
-       --datasets ${datasets} \
-       --n_hparams ${n_hparams} \
-       --n_trials ${trial}
-
-jobid=$(date +"%y%m%d%H%M%S")
-algorithm=CORAL
-current_output_dir=${outputdir}/${jobid}/${overlap}/${algorithm}_${datasets}_o${overlap}_h${n_hparams}_s${steps}_t${trial}_${jobid}
-echo starting ${current_output_dir}
-mkdir -p ${current_output_dir}
-
-echo 'y' |CUDA_VISIBLE_DEVICES=${gpu} python -m domainbed.scripts.sweep launch\
-       --data_dir=${datadir} \
-       --output_dir=${current_output_dir} \
-       --command_launcher local \
-       --overlap ${overlap} \
-       --steps ${steps} \
-       --single_test_envs \
-       --algorithms ${algorithm} \
-       --datasets ${datasets} \
-       --n_hparams ${n_hparams} \
-       --n_trials ${trial}
-
-jobid=$(date +"%y%m%d%H%M%S")
-algorithm=SelfReg
-current_output_dir=${outputdir}/${jobid}/${overlap}/${algorithm}_${datasets}_o${overlap}_h${n_hparams}_s${steps}_t${trial}_${jobid}
-echo starting ${current_output_dir}
-mkdir -p ${current_output_dir}
-
-echo 'y' |CUDA_VISIBLE_DEVICES=${gpu} python -m domainbed.scripts.sweep launch\
-       --data_dir=${datadir} \
-       --output_dir=${current_output_dir} \
-       --command_launcher local \
-       --overlap ${overlap} \
-       --steps ${steps} \
-       --single_test_envs \
-       --algorithms ${algorithm} \
-       --datasets ${datasets} \
-       --n_hparams ${n_hparams} \
-       --n_trials ${trial}
-
-jobid=$(date +"%y%m%d%H%M%S")
-algorithm=MLDG
-current_output_dir=${outputdir}/${jobid}/${overlap}/${algorithm}_${datasets}_o${overlap}_h${n_hparams}_s${steps}_t${trial}_${jobid}
-echo starting ${current_output_dir}
-mkdir -p ${current_output_dir}
-
-echo 'y' |CUDA_VISIBLE_DEVICES=${gpu} python -m domainbed.scripts.sweep launch\
-       --data_dir=${datadir} \
-       --output_dir=${current_output_dir} \
-       --command_launcher local \
-       --overlap ${overlap} \
-       --steps ${steps} \
-       --single_test_envs \
-       --algorithms ${algorithm} \
-       --datasets ${datasets} \
-       --n_hparams ${n_hparams} \
-       --n_trials ${trial}
-
-jobid=$(date +"%y%m%d%H%M%S")
-algorithm=CAD
-current_output_dir=${outputdir}/${jobid}/${overlap}/${algorithm}_${datasets}_o${overlap}_h${n_hparams}_s${steps}_t${trial}_${jobid}
-echo starting ${current_output_dir}
-mkdir -p ${current_output_dir}
-
-echo 'y' |CUDA_VISIBLE_DEVICES=${gpu} python -m domainbed.scripts.sweep launch\
-       --data_dir=${datadir} \
-       --output_dir=${current_output_dir} \
-       --command_launcher local \
-       --overlap ${overlap} \
-       --steps ${steps} \
-       --single_test_envs \
-       --algorithms ${algorithm} \
-       --datasets ${datasets} \
-       --n_hparams ${n_hparams} \
-       --n_trials ${trial}
-
-jobid=$(date +"%y%m%d%H%M%S")
-algorithm=Fishr
-current_output_dir=${outputdir}/${jobid}/${overlap}/${algorithm}_${datasets}_o${overlap}_h${n_hparams}_s${steps}_t${trial}_${jobid}
-echo starting ${current_output_dir}
-mkdir -p ${current_output_dir}
-
-echo 'y' |CUDA_VISIBLE_DEVICES=${gpu} python -m domainbed.scripts.sweep launch\
-       --data_dir=${datadir} \
-       --output_dir=${current_output_dir} \
-       --command_launcher local \
-       --overlap ${overlap} \
-       --steps ${steps} \
-       --single_test_envs \
-       --algorithms ${algorithm} \
-       --datasets ${datasets} \
-       --n_hparams ${n_hparams} \
-       --n_trials ${trial}
-
-jobid=$(date +"%y%m%d%H%M%S")
-algorithm=ARM
-current_output_dir=${outputdir}/${jobid}/${overlap}/${algorithm}_${datasets}_o${overlap}_h${n_hparams}_s${steps}_t${trial}_${jobid}
-echo starting ${current_output_dir}
-mkdir -p ${current_output_dir}
-
-echo 'y' |CUDA_VISIBLE_DEVICES=${gpu} python -m domainbed.scripts.sweep launch\
-       --data_dir=${datadir} \
-       --output_dir=${current_output_dir} \
-       --command_launcher local \
-       --overlap ${overlap} \
-       --steps ${steps} \
-       --single_test_envs \
-       --algorithms ${algorithm} \
-       --datasets ${datasets} \
-       --n_hparams ${n_hparams} \
-       --n_trials ${trial}
-
-jobid=$(date +"%y%m%d%H%M%S")
-algorithm=Transfer
-current_output_dir=${outputdir}/${jobid}/${overlap}/${algorithm}_${datasets}_o${overlap}_h${n_hparams}_s${steps}_t${trial}_${jobid}
-echo starting ${current_output_dir}
-mkdir -p ${current_output_dir}
-
-echo 'y' |CUDA_VISIBLE_DEVICES=${gpu} python -m domainbed.scripts.sweep launch\
-       --data_dir=${datadir} \
-       --output_dir=${current_output_dir} \
-       --command_launcher local \
-       --overlap ${overlap} \
-       --steps ${steps} \
-       --single_test_envs \
-       --algorithms ${algorithm} \
-       --datasets ${datasets} \
-       --n_hparams ${n_hparams} \
-       --n_trials ${trial}
+    echo 'y' |CUDA_VISIBLE_DEVICES=${gpu} python -m domainbed.scripts.sweep launch\
+           --data_dir=${datadir} \
+           --output_dir=${current_output_dir} \
+           --command_launcher local \
+           --overlap ${overlap} \
+           --steps ${steps} \
+           --single_test_envs \
+           --algorithms ${algorithm} \
+           --datasets ${datasets} \
+           --n_hparams ${n_hparams} \
+           --n_trials ${trial}
+done
