4e6cd5272afe7003739ce1f14f8cae8a7824e321
kimathikaai <kiprotich.kaai@gmail.com>
2023-04-05 12:04:41 -0400

    Setting up ablations

--- results.sh
+++ results.sh
@@ -4,7 +4,7 @@ dataset='All'
 
 for overlap in 0 33 66 100
 do
-    input_dir='~/scratch/saved/domainbed_results/'$overlap
+    input_dir='/Users/kimathikaai/scratch/saved/domainbed_results/'$overlap
     selec_metric='acc'
     eval_metric='acc'
     python -m domainbed.scripts.collect_results\
