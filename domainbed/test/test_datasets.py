26e7b496a51255e93379a8498377b65d9601cca3
Saad Hossain <s42hossa@beluga1.int.ets1.calculquebec.ca>
2023-04-05 12:01:50 -0400

    tsne plots

--- domainbed/test/test_datasets.py
+++ domainbed/test/test_datasets.py
@@ -29,12 +29,15 @@ def get_overlap_params() -> List[Tuple[str, str, int, List[int]]]:
     overlapping_classes = {
         "PACS": {0: [], 33: [2, 4], 66: [0, 2, 3, 4, 5], 100: list(range(7))},
         "VLCS": {0: [], 33: [2, 3], 66: [0, 2, 3, 4], 100: list(range(5))},
+        "WILDSCamelyon": {0: [], 33: [0, 1], 100: list(range(2))},
     }
-    num_domains = {"PACS": 4, "VLCS": 4}
+    num_domains = {"PACS": 4, "VLCS": 4, "WILDSCamelyon": 6}
 
     params = []
-    for dataset in ["PACS", "VLCS"]:
+    for dataset in ["WILDSCamelyon"]: # "PACS", "VLCS", 
         for overlap in [0, 33, 66, 100]:
+            if dataset == "WILDSCamelyon" and overlap == 66:
+                continue
             for test_env in range(num_domains[dataset]):
                 id = dataset + str(overlap)+f"_test_{test_env}"
                 params.append((id, dataset, overlap, test_env, overlapping_classes[dataset][overlap]))
