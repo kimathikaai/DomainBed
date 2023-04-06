7a1521d8948756c99a9c2d631459bff3c1f53230
kimathikaai <kiprotich.kaai@gmail.com>
2023-04-06 17:15:41 -0400

    Simplify results generator

--- results.sh
+++ results.sh
@@ -5,43 +5,18 @@ dataset='All'
 for overlap in 0 33 66 100
 do
     input_dir='/Users/kimathikaai/scratch/saved/domainbed_results/'$overlap
-    selec_metric='acc'
-    eval_metric='acc'
-    python -m domainbed.scripts.collect_results\
-        --input_dir=$input_dir \
-        --eval_metric=$eval_metric \
-        --selec_metric=$selec_metric \
-        --overlap=$overlap \
-        --dataset=$dataset \
-        --latex
 
-    selec_metric='acc'
-    eval_metric='f1'
-    python -m domainbed.scripts.collect_results\
-        --input_dir=$input_dir \
-        --eval_metric=$eval_metric \
-        --selec_metric=$selec_metric \
-        --overlap=$overlap \
-        --dataset=$dataset \
-        --latex
-
-    selec_metric='acc'
-    eval_metric='oacc'
-    python -m domainbed.scripts.collect_results\
-        --input_dir=$input_dir \
-        --eval_metric=$eval_metric \
-        --selec_metric=$selec_metric \
-        --overlap=$overlap \
-        --dataset=$dataset \
-        --latex
-
-    selec_metric='acc'
-    eval_metric='nacc'
-    python -m domainbed.scripts.collect_results\
-        --input_dir=$input_dir \
-        --eval_metric=$eval_metric \
-        --selec_metric=$selec_metric \
-        --overlap=$overlap \
-        --dataset=$dataset \
-        --latex
+    for selec_metric in acc nacc oacc f1
+    do
+        for eval_metric in acc nacc oacc f1
+        do
+            python -m domainbed.scripts.collect_results\
+                --input_dir=$input_dir \
+                --eval_metric=$eval_metric \
+                --selec_metric=$selec_metric \
+                --overlap=$overlap \
+                --dataset=$dataset \
+                --latex
+        done
+    done
 done
