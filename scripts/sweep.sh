768b0cb723ac01944b0476f3ade2b99d7c56a9c0
kimathikaai <kiprotich.kaai@gmail.com>
2023-09-05 11:11:39 -0400

    Template for running the sweep

---
+++ scripts/sweep.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# local variables
+datadir=/pub2/data
+outputdir=/pub2/podg
+n_hparams=5
+steps=5001
+trial=3
+gpu=0
+algorithm=CausIRL_MMD
+jobid=iclr2024
+
+for dataset in OfficeHome VLCS PACS
+do
+    for overlap in low high
+    do
+        curr_outdir=${outputdir}/${jobid}_${algorithm}_${dataset}_${overlap}
+        echo starting ${curr_outdir}
+        mkdir -p ${curr_outdir}
+        CUDA_VISIBLE_DEVICES=${gpu} python -m domainbed.scripts.sweep launch\
+           --data_dir=${datadir} \
+           --algorithms $algorithm \
+           --output_dir $curr_outdir\
+           --command_launcher local \
+           --overlap $overlap \
+           --steps ${steps} \
+           --single_test_envs \
+           --datasets=${dataset} \
+           --n_hparams ${n_hparams} \
+           --n_trials ${trial} \
+           --skip_confirmation
+    done
+done
+
