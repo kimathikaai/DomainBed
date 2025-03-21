3fb51e01d467163746b4540eb8f12a31a45330b2
kimathikaai <kiprotich.kaai@gmail.com>
2025-03-20 21:24:18 -0400

    fixup! Prepare for wilds dataset

--- domainbed/datasets.py
+++ domainbed/datasets.py
@@ -760,6 +760,8 @@ class DomainBedWILDSDataset(MultipleDomainDataset):
 class WILDSCamelyon(DomainBedWILDSDataset):
     ENVIRONMENTS = [ "hospital_0", "hospital_1", "hospital_2", "hospital_3",
             "hospital_4"]
+    NUM_CLASSES = 2
+    N_OC = {"1": {"N_oc": 1}}
     def __init__(self, root, test_envs, hparams, overlap: str = "3", overlap_seed=None):
         dataset = Camelyon17Dataset(root_dir=root)
         self.class_overlap = {
